c4b7dd0c53b5c8f4c3cf2f9e96dd0e40d9f578b2
lipe <fefs@cin.ufpe.br>
2023-07-22 23:04:31 -0300

    FEAT: implementating routes

--- App.js
+++ App.js
@@ -1,5 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import LandingPage from "./src/pages/landing";
+import HomePage from "./src/pages/home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <LandingPage />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Landing" component={LandingPage} />
+        <Stack.Screen name="Home" component={HomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,28 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Dimensions } from "react-native";
 
-import { Button } from "../components";
-
-import { SvgUri } from "react-native-svg";
-
 const vh = Dimensions.get("window").height;
 
-export default function LandingPage({ navigation }) {
+export default function HomePage({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.h1}>AI Puzzles</Text>
-      <Text style={styles.h2}>
-        Create your own puzzle within the reach of a touch
-      </Text>
-
-      <SvgUri
-        width="300"
-        height="300"
-        style={styles.icon}
-        uri="https://svgur.com/i/vbS.svg"
-      />
-
-      <Button title="Get started" onPress={() => navigation.navigate("Home")} />
+      <Text style={styles.h1}>Home</Text>
     </View>
   );
 }
@@ -46,11 +30,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     position: "absolute",
     zIndex: 2,
-    top: vh * 0.2,
+    top: vh * 0.3,
   },
   icon: {
     position: "absolute",
     zIndex: 1,
-    top: vh * 0.35,
+    top: vh * 0.4,
   },
 });
